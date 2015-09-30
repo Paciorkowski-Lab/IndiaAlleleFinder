dfe6557b3a9983fa9c546e4b2b98817145ef67a5
Jimmy Zhang <imyjimmy@gmail.com>
2015-09-29 21:53:05 -0400

    slightly more secure db uri path

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # import sqlite3
 from flask import Flask, url_for, request, session, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
+import base64
 
 app = Flask(__name__)
 app.secret_key = "my precious"
 
-app.config.update(SEND_FILE_MAX_AGE_DEFAULT = 240)
+# app.config.update(SEND_FILE_MAX_AGE_DEFAULT = 240)
 #DATABASE = '/Users/imyjimmy/Dropbox/for_alex/indiaAlleleFinder/db/database.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/indiaAlleleFinderDB'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://jzhang69:' + base64.b64decode("QnIwMGtseW4h") + '@mysql.imyjimmy.com/indiaallelefinderdb'
 # app.config.from_object(__name__)
 
 db = SQLAlchemy(app)
@@ -60,22 +61,46 @@ def sort_by_chr(chromosome):
 	rows = execute_query("SELECT * FROM alleles WHERE Chr=:param", {"param": chromosome}) #[chromosome]
 	return '<br>'.join(str(row) for row in rows)
 
-@app.route('/search')
+@app.route('/search', methods=['GET'])
 def search():
 	query = request.args.get('search')
 	results = processQuery(query)
-	print("results: " + str(results))
-	html = '<br>'.join(str(row) for row in results)
-	print("html: " + html)
+	# print("results: " + str(results))
+	html = makeTable(results)
+	# print("html: " + html)
 	return render_template('search.html', query=query, results=results, h=html)
 
 def processQuery(query):
 	gene_rows = execute_query("SELECT * FROM alleles WHERE GenerefGene=:param", {"param": query})
-	# print('in processQuery. length of rows: ' + str(len(gene_rows)))
-	# for row in gene_rows:
-	#  	print("genes: " + str(row))
 	return gene_rows
 
+#["(u'1'", " u'14653'", " u'14653'", " u'C'", " u'T'", " u'ncRNA_exonic'", " u'WASH7P'", " u'NA'", " u'NA'", " u'NA'", " u'Score=0.993729;Name=chr9:10843'", " u'rs62635297'", " u'NA'", " u'NA'", " u'NA'", " u'india:0.280487804878'", " u'1')"]
+def makeTable(results):
+	#very shady but okay for now.
+	#@todo: make this less hacky
+	result = {}
+	ind = 0
+	for column in results:
+		result[ind] = []
+		print "hereereee"
+		elements = str(column).split(',')
+		for e in elements:
+			result[ind].append(str(e).split("u'")[1].split("'")[0])
+		ind += 1
+
+	print "result" 
+	print result
+	compiledStr = "<table class='table-striped'><tbody>"
+	for i in range(0, ind):
+		compiledStr += '<tr>'
+		for r in result[i]:
+			compiledStr += '<td>' + r + '</td>'
+		compiledStr += '</tr>'
+	compiledStr += '</tbody></table>'
+	# return '<br>'.join(str(row).split(',') for row in results)
+	print compiledStr
+	return compiledStr
+	
 
 
 ###THE FOLLOWING IS EXAMPLE CODE###
