eb3702ca90a9691f6d9a8c139716380f6af9124c
Jimmy Zhang <imyjimmy@localhost.imac>
2015-11-30 21:52:52 -0500

    result tables are styled, minimally. results controller now handles queries by variant, gene

--- lib/IndiaAlleleFinder/Controller/Results.pm
+++ lib/IndiaAlleleFinder/Controller/Results.pm
@@ -1,6 +1,7 @@
 package IndiaAlleleFinder::Controller::Results;
 use Moose;
 use namespace::autoclean;
+use Scalar::Util qw(looks_like_number);
 
 BEGIN { extends 'Catalyst::Controller'; }
 
@@ -23,11 +24,53 @@ Catalyst Controller.
 
 sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
-
     #$c->response->body('Matched IndiaAlleleFinder::Controller::Results in Results.');
-    $c->stash(genes => [$c->model('IndiaAlleleFinderDB::Allele')->all]);
+ 	#$c->stash(genes => [$c->model('IndiaAlleleFinderDB::Allele')->all]);
+	
+	my $query = $c->request->param('search');
+	#$c->stash->{genes} = $query; #show everything
+	my $variantQuery = "ayyy";
 
+	#search by gene
+	$c->stash(genes => 
+		[$c->model('IndiaAlleleFinderDB::Allele')->search({generefgene => {'like', "$query"}})]);
+
+	#search by rsID
+	$c->stash(rsID => 
+		[$c->model('IndiaAlleleFinderDB::Allele')->search({snp138 => {'like', "$query"}})]);
+	
+	#search by variant: 22-46615880-T-C
+	my ($chromosome, $pos, $ref, $alt) = split(/-/, $query);
+	if ((looks_like_number($chromosome) or $chromosome eq 'X' or $chromosome eq 'Y') and 
+		$pos ne '' and $ref ne '' and $alt ne '') {
+		#we have a valid variant query.
+		$variantQuery = "yooo";
+		$c->stash(variantQuery => $variantQuery);
+
+		$c->stash(genes => 
+			[$c->model('IndiaAlleleFinderDB::Allele')->search({
+				chr => {'like', "$chromosome"}, 
+				start => {'like', "$pos"},
+				ref => {'like', "$ref"},
+				alt => {'like', "$alt"}
+				})])
+	}
+	
+	#search by region
+	#($chr, $region) = split(/:/, $query);
+	
+	#if 
+	#$c->stash(region => 
+	#	[$c->model('IndiaAlleleFinderDB::Allele')->search({chr => {'like', "$query.split(':')[0]"}})]);
+
+
+	$c->stash(searchText => $query);
 	$c->stash(template => 'results.tt');
+
+	#ghetto debugging here we go
+	$c->stash(chr => $chromosome);
+	$c->stash(region => $pos);
+	$c->stash(variantQuery => $variantQuery);
 }
 
 =head3 list
@@ -36,11 +79,14 @@ Fetch all results objects, pass to books/list.tt in stash to be displayed
 
 =cut
 
-sub list :Local {
+sub list :Local :Arg(0) {
 	my ($self, $c) = @_;
 
-	$c->stash(genes => [$c->model('IndiaAlleleFinderDB::Allele')->all]);
+	#print $c->request->arguments->[0];
+	
+	my $query = $c->request->param('search');
 
+	$c->stash->{genes} = 'query';
 	$c->stash(template => 'results.tt');
 }
 =encoding utf8
